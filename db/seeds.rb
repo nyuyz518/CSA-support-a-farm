f9f9653741dce1474d8d5ba5735de7b08d27c0aa
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-07-31 09:34:09 -0400

    seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,37 +6,89 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-users = User.create([
+users = [
   { name: 'Lily', user_name: "nyuyz518", password: "password1" },
   { name: 'Claudia', user_name: "claud", password: "password2" },
   { name: 'Matt', user_name: "matty", password: "password3" },
   { name: 'Jared', user_name: "jaredd", password: "password4" },
-])
+]
 
-farms = Farm.create([
-    {name: 'Green Hills Farm', location: "Melville", year_est: 1948},
-    {name: 'Hudson Valley Farm', location: "Columbia County", year_est: 1962}
-  ])
+users.each {|user| User.create(user)}
 
-products = Product.create([
-  {name: 'Yellow Squash', farm_id: farms.sample.id},
-  {name: 'Watermelon', farm_id: farms.sample.id},
-  {name: 'Eggs', farm_id: farms.sample.id}
-  ])
+farms = [
+  {name: 'Green Hills User', location: "Melville", year_est: 1948},
+  {name: 'Hudson Valley Farm', location: "Columbia County", year_est: 1962}
+]
 
-wish_list_items = WishListItem.create([
-  {item: "blueberries", user_id: users.sample.id},
-  {item: "apple", user_id: users.sample.id},
-  {item: "chicken", user_id: users.sample.id}
-  ])
+farms.each {|farm| Farm.create(farm)}
 
-share = Share.create([
-  {user_id: users.sample.id, farm_id: farms.sample.id},
-  {user_id: users.sample.id, farm_id: farms.sample.id},
-  {user_id: users.sample.id, farm_id: farms.sample.id},
-  {user_id: users.sample.id, farm_id: farms.sample.id},
-  {user_id: users.sample.id, farm_id: farms.sample.id},
-  {user_id: users.sample.id, farm_id: farms.sample.id},
-  {user_id: users.sample.id, farm_id: farms.sample.id},
-  {user_id: users.sample.id, farm_id: farms.sample.id},
-  ])
+products = [
+{name: "Arugula"},
+{name: "Basil"},
+{name: "Beets"},
+{name: "Blackberries"},
+{name: "Blueberries"},
+{name: "Broccoli"},
+{name: "Cabbage"},
+{name: "Carrots"},
+{name: "Cauliflower"},
+{name: "Celery"},
+{name: "Chard"},
+{name: "Chicory"},
+{name: "Collards"},
+{name: "Corn"},
+{name: "Cucumber"},
+{name: "Dandelions"},
+{name: "Dill"},
+{name: "Eggplant"},
+{name: "Escarole"},
+{name: "Fennel"},
+{name: "Garlic"},
+{name: "Green Beans"},
+{name: "Herbs"},
+{name: "Horseradish"},
+{name: "Kale"},
+{name: "Kohlrabi"},
+{name: "Lettuce"},
+{name: "Canteloupe"},
+{name: "Mint"},
+{name: "Mizuna"},
+{name: "Mustard Greens"},
+{name: "Onions"},
+{name: "Parsley"},
+{name: "Peaches"},
+{name: "Bell Peppers"},
+{name: "Plums"},
+{name: "Potatoes"},
+{name: "Radish"},
+{name: "Raspberries"},
+{name: "Sage"},
+{name: "Scallions"},
+{name: "Shallots"},
+{name: "Spinach"},
+{name: "Sprouts"},
+{name: "Squash"},
+{name: "Tatsoi"},
+{name: "Tomatoes"},
+{name: "Turnips"},
+{name: "Watermelon"},
+]
+
+products = products.map { |product| product.merge( { farm_id: Farm.all.sample.id } ) }
+products.each { |product| Product.create(product) }
+
+wish_list_items = [
+  {item: "blueberries"},
+  {item: "apple"},
+  {item: "chicken breasts"}
+  ]
+
+wish_list_items = wish_list_items.map { |item| item.merge( { user_id: User.all.sample.id } ) }
+wish_list_items.each { |item| WishListItem.create(item) }
+#
+# share = {user_id: User.all.sample.id, farm_id: Farm.all.sample.id}
+
+8.times do
+  Share.create({user_id: User.all.sample.id, farm_id: Farm.all.sample.id}
+)
+end
