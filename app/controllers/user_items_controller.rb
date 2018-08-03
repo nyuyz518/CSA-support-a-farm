4869db2020bbdf72357db1e2f0d9e953b4bfdaea
nyuyz518 <lilyzhangyun@gmail.com>
2018-08-03 10:51:14 -0400

    updates to user_item

--- app/controllers/user_items_controller.rb
+++ app/controllers/user_items_controller.rb
@@ -1,2 +1,30 @@
 class UserItemsController < ApplicationController
+
+  def new
+    @user_item = UserItem.new
+  end
+
+  def create
+    @user_item = UserItem.new(user_item_params)
+    if @user_item.save
+      flash[:notice] = "New item successfully added to your wish list!"
+      redirect_to profile_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    if @user_item.delete
+      flash[:notice] = "Item removed from wish list"
+    end 
+  end
+
+
+  private
+
+  def user_item_params
+    params.require(:user_item).permit(:user_id, :wish_list_item_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :user_items
+  resources :user_items, only: [:new, :create, :destroy]
   root to: 'users#show'
   resources :categories, only: [:index, :show]
   resources :users, only: [:show, :new, :create]
